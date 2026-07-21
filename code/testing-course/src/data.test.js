676293eba055a585a2c658df42b93320cdfab20f
Hpayeur <hailpaye017@gmail.com>
2026-07-21 16:48:52 -0600

    Next part

---
+++ code/testing-course/src/data.test.js
@@ -0,0 +1,17 @@
+import { describe, it, expect, vi } from "vitest";
+
+import { generateReportData } from "./data";
+
+describe("generateReportData()", () => {
+  it("should execute logFn if provided", () => {
+    const logger = vi.fn();
+
+    // logger.mockImplementation(() => {});
+    // logger.mockImplementationOnce(() => {});
+
+    generateReportData(logger);
+
+    // expect(logger).toBeCalled();
+    expect(logger).toBeCalled();
+  });
+});
